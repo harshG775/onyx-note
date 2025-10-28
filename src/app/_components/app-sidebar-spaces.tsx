58e82768c7910bdde3ba88eed4446520771e90eb
Harsh_G <hgaur491@gmail.com>
2025-10-28 22:12:46 +0530

    Refactor AppSidebarSpaces to improve structure and add active state handling for sidebar items

--- src/app/_components/app-sidebar-spaces.tsx
+++ src/app/_components/app-sidebar-spaces.tsx
@@ -1,4 +1,5 @@
-import { Folder, Plus } from "lucide-react";
+"use client";
+import { Folder, FolderOpen, Plus } from "lucide-react";
 
 import {
     SidebarGroup,
@@ -10,6 +11,7 @@ import {
     SidebarMenuItem,
 } from "@/components/ui/sidebar";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const getSpaces = () => {
     return [
@@ -17,10 +19,15 @@ const getSpaces = () => {
             title: "My Workspace",
             id: "469f18d0-a58c-4145-9faa-6e25904004d5",
         },
+        {
+            title: "Office Workspace",
+            id: "469f18d0-a58c-4145-9faa-04004d56e259",
+        },
     ];
 };
 export function AppSidebarSpaces() {
     const spaces = getSpaces();
+    const pathname = usePathname();
     return (
         <SidebarGroup>
             <SidebarGroupLabel>Spaces</SidebarGroupLabel>
@@ -30,11 +37,12 @@ export function AppSidebarSpaces() {
             <SidebarGroupContent>
                 <SidebarMenu>
                     {spaces.map((item) => {
+                        const isActive = pathname.includes(`/spaces/${item.id}`);
                         return (
                             <SidebarMenuItem key={item.title}>
-                                <SidebarMenuButton asChild>
+                                <SidebarMenuButton asChild isActive={isActive}>
                                     <Link href={`/spaces/${item.id}`}>
-                                        <Folder />
+                                        {isActive ? <FolderOpen /> : <Folder />}
                                         <span>{item.title}</span>
                                     </Link>
                                 </SidebarMenuButton>
